186cd2e94e495ea61fd0cfe17030b3c91cc7dc4a
Lameus <1vany1@mail.ru>
2023-01-31 17:58:54 +0300

    wip: add contradictiom module; todo: add readme

---
+++ expert/core/contradiction/contr_tools/NLIModel.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+
+class BERTNLIModel(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+
+        output_dim = 3
+        self.bert = model
+        
+        embedding_dim = self.bert.config.to_dict()['hidden_size']
+        self.out = nn.Linear(embedding_dim, output_dim)
+        
+    def forward(self, sequence, attn_mask, token_type):
+        
+        embedded = self.bert(
+            input_ids=sequence,
+            attention_mask=attn_mask,
+            token_type_ids=token_type
+            )[1]
+        output = self.out(embedded)
+        
+        return output
