27fb25db66c1778c80836799865c7b749dcd3b41
mdvdv <anatolmdvdv@gmail.com>
2023-03-09 15:31:40 +0300

    done: global fix congruence

--- expert/core/congruence/text_emotions/text_model.py
+++ expert/core/congruence/text_emotions/text_model.py
@@ -7,23 +7,24 @@ from typing import Dict
 
 class TextModel:
     """Emotion classification across the text channel."""
-    def __init__(self, lang: str = "EN", device: torch.device | None = None) -> None:
+
+    def __init__(self, lang: str = "en", device: torch.device | None = None) -> None:
         """
         Args:
-            lang (str): Target text language ['RU', 'EN'].
-            device (torch.device | None, optional): Object representing device type.
+            lang (str, optional): Speech language for text processing ['ru', 'en']. Defaults to 'en'.
+            device (torch.device | None, optional): Device type on local machine (GPU recommended). Defaults to None.
         """
         super().__init__()
-        assert lang in ["RU", "EN"], "Language must be 'RU' or 'EN'."
-        
+        if lang not in ["en", "ru"]:
+            raise NotImplementedError("'lang' must be 'en' or 'ru'.")
+
         self._lang = lang
         self._device = torch.device("cpu")
-        
         if device is not None:
             self._device = device
-        
+
         # Initialization of the ruBert Russian-language model for emotion classification.
-        if self._lang == "RU":
+        if self._lang == "ru":
             self.classifier = pipeline(
                 task="text-classification",
                 model="Aniemore/rubert-tiny2-russian-emotion-detection",
@@ -33,9 +34,9 @@ class TextModel:
                 return_all_scores=True,
                 device=self._device
             )
-        
+
         # Initialization of the DistilRoBERTa English-language model for emotion classification.
-        if self._lang == "EN":
+        if self._lang == "en":
             self.classifier = pipeline(
                 task="text-classification",
                 model="j-hartmann/emotion-english-distilroberta-base",
@@ -45,35 +46,39 @@ class TextModel:
                 return_all_scores=True,
                 device=self._device
             )
-    
+
     @property
     def device(self) -> torch.device:
-        """Check the device type."""
+        """Check the device type.
+
+        Returns:
+            torch.device: Device type on local machine.
+        """
         return self._device
-    
+
     @torch.no_grad()
     def predict(self, text: str) -> Dict:
         """Predicting emotions from the text in English or Russian.
-        
+
         Args:
             text (str): The text to classify.
         """
         assert isinstance(text, str), "Text must be string type."
-        
+
         emotion_dict = {"anger": 0., "neutral": 0., "happiness": 0.}
-        
-        if self._lang == "RU":
+
+        if self._lang == "ru":
             output = self.classifier(text)
-            
+
             emotion_dict["anger"] = output[0][5]["score"]
             emotion_dict["neutral"] = output[0][0]["score"]
             emotion_dict["happiness"] = output[0][1]["score"]
-        
-        elif self._lang == "EN":
+
+        elif self._lang == "en":
             output = self.classifier(text)
-            
+
             emotion_dict["anger"] = output[0][0]["score"]
             emotion_dict["neutral"] = output[0][4]["score"]
             emotion_dict["happiness"] = output[0][3]["score"]
-        
+
         return emotion_dict
