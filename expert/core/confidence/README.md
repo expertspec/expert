3abb423197898235fbc1e7be912bbf7711267a6b
Lameus <1vany1@mail.ru>
2023-03-23 12:42:32 +0300

    add readme for the modules

---
+++ expert/core/confidence/README.md
@@ -0,0 +1,29 @@
+# Confidence analysis
+Module for evaluating the level of confidence with non-verbal (facial micro-expressions and audio features) features analysis.
+
+### About
+
+The module evaluates the level of confidence with analysis of the non-verbal features of the speaker's from the picked video. The main class called ConfidenceDetector is located in the <b>confidence_analysis.py</b>. This class gets as input the path to the video and the results of feature extraction. Optionally, it is possible to specify the speaker with the setting of the specified image.
+The executable function of the ContradictionDetector is get_confidence. It returns the path to the report, which contains the list with records in the next form:
+{'time_sec' : time, 'confidence' : "No face" (in case, if there no face (couldn't be detected) in the fragment) | float (the level of confidence)}
+
+
+### The example of the usage
+
+```python
+from expert.core.confidence import confidence_analysis
+conf_det = confidence_analysis.ConfidenceDetector(video_path=video_path,
+                                                    features_path="./videos/temps/report.json",
+                                                    face_image="./videos/temps/0.jpg",
+                                                    path_to_diarization="./videos/temps/diarization.json",
+                                                    device = torch.device("cuda"),
+                                                    output_dir="./check")
+
+Confidence.get_confidence()
+```
+*Result:* './check/confidence.json'
+
+### Weights
+Here are two options for the models(different configs): only micro-expressions (landmarks_transformer) and with audio features (recommended and set as default) (landmarks_audio_transformer).
+
+- The weights of the model should be localted in the `weights/` 
--- expert/core/confidence/confidence_analysis.py
+++ expert/core/confidence/confidence_analysis.py
@@ -17,65 +17,57 @@ from expert.core.confidence.liedet.models.registry import build
 class ConfidenceDetector:
     def __init__(
         self,
-        path_to_video: str,
-        path_to_report: str,
-        path_to_image: str,
-        path_to_diarization: str,
+        video_path: str,
+        features_path: str,
+        face_image: str,
+        diarization_path: str,
         device: torch.device | None = None,
         output_dir: str | PathLike | None = None,
     ) -> None:
         """Determination of expert's confidence.
 
         Args:
-            path_to_video (str): Path to original video.
-            path_to_report (str): Path to JSON file with information about detected faces.
-            path_to_image (str): Path to image selected by user.
-            path_to_diarization (str): Path to JSON file with diarization information.
-            output_dir (str): Path to save JSON with information emotions and congruence.
+            video_path (str): Path to original video.
+            features_path (str): Path to the result of feature extraction module.
+            face_image (str): Path to the face image selected by user.
+            diarization_path (str): Path to the result of diarization module.
+            output_dir (str): Path to the folder for saving results. Defaults to None.
 
-        Returns:
-            str: Path to JSON file with information about confidence.
         """
         self._device = torch.device("cpu")
         if device is not None:
             self._device = device
 
-        self.path_to_video = path_to_video
-        self.path_to_report = path_to_report
-        self.path_to_image = path_to_image
-        self.path_to_diarization = path_to_diarization
+        self.video_path = video_path
+        self.features_path = features_path
+        self.face_image = face_image
+        self.diarization_path = diarization_path
 
         if output_dir is not None:
             self.temp_path = output_dir
         else:
-            basename = os.path.splitext(os.path.basename(path_to_video))[0]
+            basename = os.path.splitext(os.path.basename(video_path))[0]
             self.temp_path = os.path.join("temp", basename)
         if not os.path.exists(self.temp_path):
             os.makedirs(self.temp_path)
 
     def convert(self, target_fps=30):
         # Get fps info.
-        probe = ffmpeg.probe(self.path_to_video)
-        video_info = next(
-            s for s in probe["streams"] if s["codec_type"] == "video"
-        )
+        probe = ffmpeg.probe(self.video_path)
+        video_info = next(s for s in probe["streams"] if s["codec_type"] == "video")
         orig_fps = int(video_info["r_frame_rate"].split("/")[0])
         if orig_fps != target_fps:
             try:
-                input = ffmpeg.input(self.path_to_video)
+                input = ffmpeg.input(self.video_path)
                 video = input.video
                 audio = input.audio
                 video = ffmpeg.filter(video, "fps", fps=target_fps, round="up")
-                converted_name = os.path.basename(self.path_to_video).split(".")
-                converted_name = (
-                    converted_name[0] + "_conv" + "." + converted_name[1]
-                )
+                converted_name = os.path.basename(self.video_path).split(".")
+                converted_name = converted_name[0] + "_conv" + "." + converted_name[1]
                 out = ffmpeg.output(
                     video,
                     audio,
-                    os.path.join(
-                        os.path.dirname(self.path_to_video), converted_name
-                    ),
+                    os.path.join(os.path.dirname(self.video_path), converted_name),
                 )
                 ffmpeg.run(out, capture_stderr=True)
 
@@ -86,16 +78,14 @@ class ConfidenceDetector:
                 raise conv_error
         else:
             print("Video has appropriate fps")
-            return self.path_to_video
+            return self.video_path
 
     def get_confidence(self):
         cfg = "./expert/core/confidence/configs/landmarks_audio_transformer.py"
-        video_path = self.path_to_video
+        video_path = self.video_path
 
-        face_report = pd.read_json(self.path_to_report)
-        expert_idx = int(
-            os.path.splitext(os.path.basename(self.path_to_image))[0]
-        )
+        face_report = pd.read_json(self.features_path)
+        expert_idx = int(os.path.splitext(os.path.basename(self.face_image))[0])
         face_report = face_report[
             face_report["speaker_by_video"] == expert_idx
         ].reset_index(drop=True)
@@ -106,7 +96,7 @@ class ConfidenceDetector:
             .index[0]
         )
 
-        with open(self.path_to_diarization, "r") as f:
+        with open(self.diarization_path, "r") as f:
             stamps = json.load(f)
 
         stamps = stamps[key]
@@ -143,17 +133,11 @@ class ConfidenceDetector:
                         ]
                         temporary.unsqueeze_(dim=0)
                         sample["audio_frames"] = temporary.to("cuda")
-                    sample["video_frames"] = sample["video_frames"].to(
-                        self._device
-                    )
-                    sample["audio_frames"] = sample["audio_frames"].to(
-                        self._device
-                    )
+                    sample["video_frames"] = sample["video_frames"].to(self._device)
+                    sample["audio_frames"] = sample["audio_frames"].to(self._device)
                     predict = runner.predict_sample(sample)
                     if isinstance(predict, str):
-                        report.append(
-                            {"time_sec": current_time, "confidence": predict}
-                        )
+                        report.append({"time_sec": current_time, "confidence": predict})
                     else:
                         report.append(
                             {
@@ -173,9 +157,9 @@ class ConfidenceDetector:
                 except Exception:
                     sample = vr[start:]
                 if sample["video_frames"].size()[0] != cfg.window:
-                    sample["video_frames"] = sample["video_frames"][
-                        : cfg.window
-                    ].to(self._device)
+                    sample["video_frames"] = sample["video_frames"][: cfg.window].to(
+                        self._device
+                    )
                     temporary = sample["audio_frames"][0][
                         : cfg["window_secs"] * cfg["audio_fps"]
                     ].to(self._device)
@@ -186,9 +170,7 @@ class ConfidenceDetector:
                 predict = runner.predict_sample(sample)
                 current_time = 0
                 if isinstance(predict, str):
-                    report.append(
-                        {"time_sec": current_time, "confidence": predict}
-                    )
+                    report.append({"time_sec": current_time, "confidence": predict})
                 else:
                     report.append(
                         {
@@ -201,9 +183,7 @@ class ConfidenceDetector:
                 current_time += cfg.window / cfg.video_fps
 
         # Save results to temporary path.
-        with open(
-            os.path.join(self.temp_path, "confidence.json"), "w"
-        ) as filename:
+        with open(os.path.join(self.temp_path, "confidence.json"), "w") as filename:
             json.dump(report, filename)
 
         return os.path.join(self.temp_path, "confidence.json")
