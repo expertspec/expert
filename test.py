0dbd9df43c7a659c8664c67deddff6fbb4f893fd
ilyushka <61294398+DRMPN@users.noreply.github.com>
2023-05-01 03:48:54 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+def test() :
+  extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_='test'
+  pass
+def test1():
+   pass
+def test2():
+  print('hello')
