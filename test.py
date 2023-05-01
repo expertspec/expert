5505f27248eceb5811a3dec604439e1d64033ac5
ilyushka <61294398+DRMPN@users.noreply.github.com>
2023-05-01 03:51:04 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def test() :
-  extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_extreamly_long_='test'
-  pass
-def test1():
-   pass
-def test2():
-  print('hello')
