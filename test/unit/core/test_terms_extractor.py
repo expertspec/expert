2c3ed2e5fc2620a1aeb0f03c008238f94b6ea65b
Ivan Smirnov <34346831+Lameus@users.noreply.github.com>
2023-08-28 15:52:25 +0300

    Update test_terms_extractor.py
    fixed term test

--- test/unit/core/test_terms_extractor.py
+++ test/unit/core/test_terms_extractor.py
@@ -5,33 +5,33 @@ from expert.core.terms_extractor.get_terms import TermsExtractor
 
 @pytest.fixture()
 def ru_text():
-    text = "В этом тексте содержится один термин - паттерн"
+    ru_text = "В этом тексте содержится один термин - паттерн"
 
-    return text
+    return ru_text
 
 
 @pytest.fixture()
 def en_text():
-    text = "This text contains one term - pattern"
+    en_text = "This text contains one term - oscillation"
 
-    return text
+    return en_text
 
 
 def test_ru_text_term_extractor(ru_text):
-    extractor = TermsExtractor(ru_text, lang='ru')
+    extractor = TermsExtractor(ru_text, lang="ru")
     terms = extractor.extract_terms()
 
     assert len(terms) == 1
-    assert terms[0].term == "паттерн"
-    assert terms[0].start_pos == 39
-    assert terms[0].end_pos == 46
+    assert terms[0]["term"] == "паттерн"
+    assert terms[0]["start_pos"] == 39
+    assert terms[0]["end_pos"] == 46
 
 
 def test_en_text_term_extractor(en_text):
-    extractor = TermsExtractor(en_text, lang='en')
+    extractor = TermsExtractor(en_text, lang="en")
     terms = extractor.extract_terms()
 
     assert len(terms) == 1
-    assert terms[0].term == "pattern"
-    assert terms[0].start_pos == 30
-    assert terms[0].end_pos == 37
+    assert terms[0]["term"] == "oscillation"
+    assert terms[0]["start_pos"] == 30
+    assert terms[0]["end_pos"] == 41
